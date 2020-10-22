d0873835f4832390047ef2fac3b31b24d6f72281
wuke5 <wuke5@xiaomi.com>
2020-10-22 10:09:11 +0800

    aop

---
+++ src/main/java/pers/wk/spring/UserService.java
@@ -0,0 +1,15 @@
+package pers.wk.spring;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private String getUser() {
+        String result = "user1";
+        return result;
+    };
+
+    private void addUser(String userInfo) {
+    };
+}
